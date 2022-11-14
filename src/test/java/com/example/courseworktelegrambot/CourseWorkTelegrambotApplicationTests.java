a055af29514aa8d8d855d14605351995d5858870
Anton <imewokrus@gmail.com>
2022-11-14 17:45:56 +0300

    Initial commit

---
+++ src/test/java/com/example/courseworktelegrambot/CourseWorkTelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseworktelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkTelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
